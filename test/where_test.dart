be1f31e580d9fd626ecf4372c12d3fbee476efbb
Jiajun Yan <yanjiajun@ifanr.com>
2020-08-19 20:07:46 +0800

    feat: 添加 where 单元测试

--- test/where_test.dart
+++ test/where_test.dart
@@ -8,7 +8,6 @@ void main() {
   num randomNum2;
   List<num> randomNumArray;
   String randomString;
-  BaseRecord product;
 
   setUpAll(() {
     randomNumbers = genRandomNumbers(100, 3);
@@ -16,11 +15,6 @@ void main() {
     randomNum1 = randomNumbers[0];
     randomNum2 = randomNumbers[1];
     randomString = faker.lorem.word();
-  });
-
-  setUp(() {
-    Map<String, dynamic> recordInfo = {'id': randomNum1.toString()};
-    product = new BaseRecord.withInfo(recordInfo);
   });
 
   test('Compare', () {
@@ -264,6 +258,267 @@ void main() {
           },
           {
             'amount': {'\$exists': false}
+          }
+        ]
+      }),
+    );
+  });
+
+  test('include', () {
+    Where where = new Where();
+    GeoPoint point = new GeoPoint(randomNum1, randomNum2);
+
+    where.include('geoField', point);
+
+    expect(
+      where.condition,
+      equals(
+        {
+          '\$and': [
+            {
+              'geoField': {
+                '\$intersects': {
+                  'type': 'Point',
+                  'coordinates': [randomNum1, randomNum2],
+                }
+              }
+            }
+          ]
+        },
+      ),
+    );
+  });
+
+  test('within', () {
+    Where where = new Where();
+    List<List<num>> random2DArray = [];
+    for (var i = 0; i < 5; i++) {
+      random2DArray.add(genRandomNumbers(100, 2));
+    }
+    GeoPolygon polygon = new GeoPolygon(coordinates: random2DArray);
+    where.within('geoField', polygon);
+
+    expect(
+      where.condition,
+      equals(
+        {
+          '\$and': [
+            {
+              'geoField': {
+                '\$within': {
+                  'type': 'Polygon',
+                  'coordinates': [random2DArray],
+                }
+              }
+            }
+          ]
+        },
+      ),
+    );
+  });
+
+  test('withinCircle', () {
+    Where where = new Where();
+    GeoPoint point = new GeoPoint(randomNum1, randomNum2);
+
+    where.withinCircle('geoField', point, randomNum1);
+
+    expect(
+      where.condition,
+      equals(
+        {
+          '\$and': [
+            {
+              'geoField': {
+                '\$center': {
+                  'radius': randomNum1,
+                  'coordinates': [randomNum1, randomNum2],
+                }
+              }
+            }
+          ]
+        },
+      ),
+    );
+  });
+
+  test('withinRegion', () {
+    Where where = new Where();
+    GeoPoint point = new GeoPoint(randomNum1, randomNum2);
+
+    where.withinRegion('geoField', point);
+
+    expect(
+      where.condition,
+      equals(
+        {
+          '\$and': [
+            {
+              'geoField': {
+                '\$nearsphere': {
+                  'geometry': {
+                    'type': 'Point',
+                    'coordinates': [randomNum1, randomNum2]
+                  },
+                  'min_distance': 0
+                }
+              }
+            }
+          ]
+        },
+      ),
+    );
+  });
+
+  test('static and', () {
+    Where where1 = new Where();
+    Where where2 = new Where();
+    where1.contains('name', randomString);
+    where2.isNull('price');
+    Where andWhere = new Where();
+    andWhere.and([where1, where2]);
+    expect(
+      andWhere.condition,
+      equals({
+        '\$and': [
+          {
+            '\$and': [
+              {
+                'name': {'\$contains': randomString}
+              }
+            ]
+          },
+          {
+            '\$and': [
+              {
+                'price': {'\$isnull': true}
+              }
+            ]
+          }
+        ]
+      }),
+    );
+  });
+
+  test('static or', () {
+    Where where1 = new Where();
+    Where where2 = new Where();
+    where1.contains('name', randomString);
+    where2.isNull('price');
+    Where orWhere = new Where();
+    orWhere.or([where1, where2]);
+    expect(
+      orWhere.condition,
+      equals({
+        '\$or': [
+          {
+            '\$and': [
+              {
+                'name': {'\$contains': randomString}
+              }
+            ]
+          },
+          {
+            '\$and': [
+              {
+                'price': {'\$isnull': true}
+              }
+            ]
+          }
+        ]
+      }),
+    );
+  });
+
+  test('static and && or', () {
+    Where where1 = new Where();
+    Where where2 = new Where();
+    where1.contains('name', randomString);
+    where2.isNull('price');
+
+    Where orWhere = new Where();
+    orWhere.or([where1, where2]);
+
+    Where andWhere = new Where();
+    Where where3 = new Where();
+    where3.isNotNull('name');
+    andWhere.and([orWhere, where3]);
+
+    expect(andWhere.condition, {
+      '\$and': [
+        {
+          '\$or': [
+            {
+              '\$and': [
+                {
+                  'name': {'\$contains': randomString}
+                }
+              ]
+            },
+            {
+              '\$and': [
+                {
+                  'price': {'\$isnull': true}
+                }
+              ]
+            }
+          ]
+        },
+        {
+          '\$and': [
+            {
+              'name': {'\$isnull': false}
+            }
+          ]
+        }
+      ]
+    });
+  });
+
+  test('object - hasKey', () {
+    Where where = new Where();
+    where.hasKey('objectField', 'key1');
+
+    expect(
+      where.condition,
+      equals({
+        '\$and': [
+          {
+            'objectField': {'\$has_key': 'key1'}
+          }
+        ]
+      }),
+    );
+  });
+
+  test('object - eq', () {
+    Where where = new Where();
+    where.compare('objectField', '=', {'a': randomNumArray, 'b': randomNum1});
+
+    expect(
+      where.condition,
+      equals({
+        '\$and': [
+          {
+            'objectField': {
+              '\$eq': {'a': randomNumArray, 'b': randomNum1}
+            }
+          }
+        ]
+      }),
+    );
+  });
+
+  test('object - isnull', () {
+    Where where = new Where();
+    where.isNull('objectField');
+
+    expect(
+      where.condition,
+      equals({
+        '\$and': [
+          {
+            'objectField': {'\$isnull': true}
           }
         ]
       }),
