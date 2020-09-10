5d1fd89af52f68b9c39ee4c58fbd4317038cada9
Jiajun Yan <domo36k@gmail.com>
2020-09-10 11:59:03 +0800

    feat: 添加 LICENSE 和 CHANGELOG 等文件

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,9 +3,7 @@ import 'package:minapp/minapp.dart' as BaaS;
 import 'pages/index.dart';
 
 void main() {
-  BaaS.init(
-    'a4d2d62965ddb57fa4d6',
-  );
+  BaaS.init('a4d2d62965ddb57fa4d6');
 
   runApp(MaterialApp(
     title: '测试应用',
